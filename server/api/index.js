fe738b19e92ea3f544528ce43241def410907883
Samuel RioTz <samuelriotz@Samuels-MacBook-Pro.local>
2019-01-08 08:14:47 -0400

    configuracion de api

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,9 @@
 exports.setApi = app => {
-    app.get('/', (req, client) => {
-        client.send("hello world!")
-    })
-    app.get('/*', (req, client) => {
+    app.get('/api', (req, client) => {
         console.log(req.query)
-        client.json(req.query)
+        client.send(req.query)
+    })
+    app.get('/api/version', (req, client) => {
+        client.send("v 1.0")
     })
 }
