156031f681ea8e95612c9cacb832def14ce9320f
Julia1297 <valita129@gmail.com>
2019-01-24 15:03:02 -0400

    Formularios moderador

--- angular/src/app/moderador/moderador.component.ts
+++ angular/src/app/moderador/moderador.component.ts
@@ -2,6 +2,8 @@ import { Moderador } from '../../models/moderador';
 import { Component, OnInit } from '@angular/core';
 
 import { HttpService } from './../../services/http.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { Http } from '@angular/http';
 @Component({
   selector: 'app-moderador',
@@ -9,14 +11,30 @@ import { Http } from '@angular/http';
   styleUrls: ['./moderador.component.css']
 })
 export class ModeradorComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
   moderador: Moderador;
-  constructor(private http: HttpService, ) {
+
+  constructor(private http: HttpService,private formBuilder: FormBuilder ) {
     this.moderador = new Moderador();
   }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      nombreMercado: ['', Validators.required],
+      cantidadJugadores: [2, Validators.required]
+
+  });
   }
+  get f() { return this.registerForm.controls; }
   onClickMe() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {return;}
+    this.moderador.nombreMercado=this.registerForm.value.nombreMercado
+    this.moderador.cantidadJugadores=this.registerForm.value.cantidadJugadores
+
     this.http.game.createGame( this.moderador, (response) => {
       if (response.message && response.message == "ok")
         alert(`Creado correctamente, Codigo: ${response.token}`)
