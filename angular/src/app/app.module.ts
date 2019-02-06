bf5a3623ed3d1602565098a5573b331d8d4c2830
Samuel Riotz <samuelriotz@gmail.com>
2019-02-06 08:25:04 -0400

    change socket

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -31,7 +31,7 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { ThousandsPipe } from './separador-de-miles/thousand.pipe';
 
 
-const config: SocketIoConfig = { url: 'http://localhost:8080', options: {} };
+const config: SocketIoConfig = { url: 'http://67.205.156.58', options: {} };
 @NgModule({
    declarations: [
       AppComponent,
