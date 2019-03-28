f484068e87da6281f58e9eb447dddd42e11b0d2a
Samuel <samuelriotz@hotmail.com>
2019-03-28 14:27:19 -0400

    release

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const cors = require('cors')
-const port = 8080
+const port = 8075
 const api = require('./api')
 const io = require('socket.io')(server);
 
